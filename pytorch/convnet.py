ef09f588b8963f7ab34fd2a37d9bf50248b4e3cb
sadiredwan <sadi.redwan@gmail.com>
2020-03-21 17:42:11 +0600

    .

--- pytorch/convnet.py
+++ pytorch/convnet.py
@@ -35,8 +35,8 @@ class DvC():
 
 		np.random.shuffle(self.training_data)
 		np.save("training_data.npy", self.training_data)
-		print("Cats: ", self.catcount)
-		print("DOGS: ", self.dogcount)
+		# print("Cats: ", self.catcount)
+		# print("DOGS: ", self.dogcount)
 
 
 import torch
@@ -47,7 +47,7 @@ import torch.optim as optim
 class Net(nn.Module):
 	def __init__(self):
 		super().__init__()
-		self.conv1 = nn.Conv2d(1, 32, 7) # input is 1 image, 32 output channels, 7x7 kernel/window
+		self.conv1 = nn.Conv2d(1, 32, 7) #1 image, 32 output channels, 7x7 kernel/window
 		self.conv2 = nn.Conv2d(32, 64, 7)
 		self.conv3 = nn.Conv2d(64, 128, 7)
 
@@ -63,7 +63,7 @@ class Net(nn.Module):
 		x = Func.max_pool2d(Func.relu(self.conv2(x)), (2, 2))
 		x = Func.max_pool2d(Func.relu(self.conv3(x)), (2, 2))
 
-		print(x[0].shape)
+		# print(x[0].shape) #torch.Size([128, 2, 2])
 		if self._to_linear is None:
 			self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
 			return x
@@ -87,8 +87,45 @@ if __name__ == '__main__':
 	optimizer = optim.Adam(net.parameters(), lr=0.001)
 	loss_function = nn.MSELoss()
 
-	# training_data =	np.load(training_data.npy)
+	training_data =	np.load("training_data.npy", allow_pickle=True)
 
-	# X = torch.Tensor([i[0] for i in training_data]).view(-1, 64, 64)
-	# X = X/255.0
-	# y = torch.Tensor([i[1] for i in training_data])
+	X = torch.Tensor([i[0] for i in training_data]).view(-1, 64, 64)
+	X = X/255.0
+	y = torch.Tensor([i[1] for i in training_data])
+
+	VALIDATION_PCT = 0.5
+	validation_size = int(len(X)*VALIDATION_PCT)
+	# print(validation_size)
+	train_X = X[: -validation_size]
+	train_y = y[: -validation_size]
+
+	test_X = X[-validation_size :]
+	test_y = y[-validation_size :]
+
+	BATCH_SIZE = 10
+	EPOCHS = 3
+
+	# for epoch in range(EPOCHS):
+	# 	for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+	# 		batch_X = train_X[i: i+BATCH_SIZE].view(-1, 1, 50, 50)
+	# 		batch_y = train_y[i: i+BATCH_SIZE]
+
+	# 		net.zero_grad()
+	# 		outputs = net(batch_X)
+	# 		loss = loss_function(outputs, batch_y)
+	# 		loss.backward()
+	# 		optimizer.step()
+
+	# correct = 0
+	# total = 0
+	# with torch.no_grad():
+	# 	for i in tqdm(range(len(test_X))):
+	# 		accurate = torch.argmax(test_y[i])
+	# 		net_out = net(test_X[i].view(-1, 1, 64, 64))[0]  # returns a list, 
+	# 		predicted = torch.argmax(net_out)
+
+	# 		if predicted == accurate:
+	# 			correct += 1
+	# 		total += 1
+	# print("Accuracy: ", round(correct/total, 3))
+
