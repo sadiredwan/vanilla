2e18ca577b5212250db27c248754fb99234c9633
sadiredwan <sadi.redwan@gmail.com>
2020-03-27 13:23:48 +0600

    .

--- pytorch/autograd_torch.py
+++ pytorch/autograd_torch.py
@@ -1,32 +1,36 @@
 import torch
-
-#f = 2 * x
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
-
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
-
-def forward(x):
-	return w * x
-
-def loss(y, y_pred):
-	return ((y_pred - y)**2).mean()
+import torch.nn as nn
 
 if __name__ == '__main__':
-	print(f'Prediction before training: f(5) = {forward(5):.3f}')
+
+	X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+	Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+
+	n_samples, n_features = X.shape
+	print(f'#samples: {n_samples}, #features: {n_features}')
+	X_test = torch.tensor([5], dtype=torch.float32)
+
+	input_size = n_features
+	output_size = n_features
+	model = nn.Linear(input_size, output_size)
+
+	print(f'Prediction before training: f(5) = {model(X_test).item():.3f}')
 
 	learning_rate = 0.01
-	n_iters = 20
+	n_iters = 100
+
+	loss = nn.MSELoss()
+	optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 	for epoch in range(n_iters):
-		y_pred = forward(X)
-		l = loss(Y, y_pred)
-		l.backward() #dl/dw
-		with torch.no_grad():
-			w -= learning_rate * w.grad
+		y_predicted = model(X)
+		l = loss(Y, y_predicted)
+		l.backward()
+		optimizer.step()
+		optimizer.zero_grad()
 
-		w.grad.zero_()
-		if epoch % 2 == 0:
-			print(f'epoch {epoch+1}: w = {w:.3f}, loss = {l:.8f}')
-		 
-	print(f'Prediction after training: f(5) = {forward(5):.3f}')
+		if epoch % 10 == 0:
+			[w, b] = model.parameters() # unpack parameters
+			print('epoch ', epoch+1, ': w = ', w[0][0].item(), ' loss = ', l)
+
+	print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
